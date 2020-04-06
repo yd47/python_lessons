d4b519bf5e1e9d8ba58d2f651ccfb88441fe85fa
yd47 <gerdsher@yandex.ru>
2020-04-06 20:28:24 +0500

    task 4.6.1

--- pyneng-examples-exercises/exercises/04_data_structures/task_4_6_1.py
+++ pyneng-examples-exercises/exercises/04_data_structures/task_4_6_1.py
@@ -17,16 +17,18 @@ Outbound Interface:    FastEthernet0/0
 ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
 var = ospf_route.split()
 
-template = '''
-	Protocol: {Prot:>15}
-	Prefix: {Prefix:>25}
-	AD/Metric: {Metric:>16}
-	Next-Hop: {NextHop:>21}
-	Last update: {Update:>14}
-	Outbound Interface: {OIF:>16}
+template = ['Protocol: {Prot:>15}',
+	'Prefix: {Prefix:>25}',
+	'AD/Metric: {Metric:>16}',
+	'Next-Hop: {NextHop:>21}',
+	'Last update: {Update:>14}',
+	'Outbound Interface: {OIF:>16}']
+
+
+'''
+print(template.format(Prot=var[0].replace('O', 'OSPF'), Prefix=var[1], 
+Metric=(var[2].strip('[]')), NextHop=var[4], Update=var[5], OIF=var[6]))
 '''
 
-
-
-print(template.format(Prot=var[0].replace('O', 'OSPF'), Prefix=var[1], 
+print('\n'.join(template).format(Prot=var[0].replace('O', 'OSPF'), Prefix=var[1], 
 Metric=(var[2].strip('[]')), NextHop=var[4], Update=var[5], OIF=var[6]))
