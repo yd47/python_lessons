bd0c46a877dbc00cb05555c81ad5c9aa824e7894
yd47 <gerdsher@yandex.ru>
2020-05-05 01:40:02 +0500

    Сделал задание 5.3а

--- pyneng-examples-exercises/exercises/05_basic_scripts/task_5_3a.py
+++ pyneng-examples-exercises/exercises/05_basic_scripts/task_5_3a.py
@@ -21,3 +21,23 @@ trunk_template = [
     'switchport trunk encapsulation dot1q', 'switchport mode trunk',
     'switchport trunk allowed vlan {}'
 ]
+
+
+dict0 = {'access': 'Введите номер VLAN: ', 'trunk': 'Введите разрешенные VLANы: '}  
+
+
+mode = input('Введите режим работы интерфейса (access/trunk): ')
+ifn = input('Введите тип и номер интерфейса: ')
+vl0 = dict0.get(mode) #почему не подставлялось через dict0[mode] я хз, почему то ругается, что это строка
+vl = input(vl0)
+
+acctmp = ((str('\n'.join(access_template))).format(vl))
+trtmp = ((str('\n'.join(trunk_template))).format(vl))
+
+#затем делаем словарик
+dict1 = dict([('access', acctmp),('trunk', trtmp)])
+
+
+print('interface {}'.format(ifn))
+print(dict1[mode])
+
